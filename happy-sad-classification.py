991bef9879d0a2202ee390ffa4898ff6ffbb7e31
Amany Gaber <116579137+amanygaber330@users.noreply.github.com>
2023-12-24 19:01:26 +0200

    Update happy-sad-classification.py

--- happy-sad-classification.py
+++ happy-sad-classification.py
@@ -12,57 +12,54 @@ except Exception as e:
 
 # Define the Streamlit app
 def app():
-    # Set the app title and page layout
-    st.set_page_config(
-        page_title="Happy or Sad Classifier",
-        page_icon="😊",
-        layout="centered"
-    )
+    # Set the app title
+    st.title('Pic Happy or Sad 🤔 ?!')
 
-    # Set a more suitable title
-    st.title('picture Happy or Sad')
-
-    # Add a centered form for uploading an image
-    uploaded_file = st.file_uploader("Upload an image for emotion classification", type=["jpg", "jpeg", "png"])
+    # Add a file uploader widget
+    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 
     # Check if an image has been uploaded
     if uploaded_file is not None:
-        # Read the image data
-        image = Image.open(uploaded_file)
+        if uploaded_file.type in ["image/jpg", "image/jpeg", "image/png"]:
+            # Read the image data
+            image = Image.open(uploaded_file)
 
-        # Resize the image to match the expected input shape of the model
-        image = image.resize((256, 256))
+            # Resize the image to match the expected input shape of the model
+            image = image.resize((256, 256))
 
-        # Convert the image to a numpy array
-        image_array = np.array(image)
+            # Convert the image to a numpy array
+            image_array = np.array(image)
 
-        # Normalize the pixel values
-        image_array = image_array / 255.0
+            # Normalize the pixel values
+            image_array = image_array / 255.0
 
-        # Add a batch dimension
-        image_array = np.expand_dims(image_array, axis=0)
+            # Add a batch dimension
+            image_array = np.expand_dims(image_array, axis=0)
 
-        # Make predictions on the image with error handling
-        try:
-            predictions = model.predict(image_array)
-            threshold = 1
-            predicted_class_name = 'Sad 🥺' if predictions[0, 0] >= threshold else 'Happy'
-        except Exception as e:
-            st.error(f"Error during prediction: {e}")
-            st.stop()
+            # Make predictions on the image with error handling
+            try:
+                predictions = model.predict(image_array)
+                threshold = 0.5
+                if predictions[0, 0] <= threshold:
+                    predicted_class_name = 'sad🥺'
+                else:
+                    predicted_class_name = 'happy😄'
+            except Exception as e:
+                st.error(f"Error during prediction: {e}")
+                st.stop()
 
-        # Display the image and the predicted class name
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+            # Display the image and the predicted class name
+            st.image(image, caption='', use_column_width=True)
 
-        # Add a separator for a cleaner look
-        st.markdown("---")
+            # Add a separator
+            st.write('---')
 
-        # Display the predicted class name with improved styling
-        st.markdown(f'## Prediction: {predicted_class_name}')
+            # Display the predicted class name
+            st.markdown(f'## {predicted_class_name.capitalize()}')
 
-        # Display confidence score
-        confidence_score = predictions[0, 0] if predicted_class_name == 'Sad 🥺' else 1 - predictions[0, 0]
-        #st.write(f"Confidence Score: {confidence_score:.2%}")
+            # Display confidence score
+            confidence_score = predictions[0, 0] if predicted_class_name == 'sad🥺' else 1 - predictions[0, 0]
+            #st.write(f"Confidence Score: {confidence_score:.2%}")
 
 # Run the app
 if __name__ == "__main__":
